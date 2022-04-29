0e09994a026f5b3c7aac68e5fdf99944a153418b
krupabahulya <krupabahulya.y@gmail.com>
2022-04-29 17:00:01 +0200

    query params working both with ingredients and products

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,12 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import Product from "../models/product.js";
 
-export const getAllProducts = asyncHandler(async (req, res, next) =>
-  res.send("GET All")
-);
+export const getAllProducts = asyncHandler(async (req, res, next) =>{
+  console.log(req.query);
+const products = await Product.find(req.query);
+
+res.send(products)
+});
 
 export const getSingleProduct = asyncHandler(async (req, res, next) =>
   res.send("GET single")
