3a04531f6cf0a6ed01e86c03a33aeaa1650b5101
Natasha Enriquez <95707854+TashaEnriquez@users.noreply.github.com>
2022-05-06 10:11:41 +0200

    draft of routine section

--- models/User.js
+++ models/User.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
-const { Schema, model } = mongoose;
+const { Schema, model, ObjectId } = mongoose;
 
-const userSchema = new Schema({
-    name: { type: String, required: [true, 'Name is required'] },
-    email: { type: String, required: [true, 'Email is required'] },
-    password: { type: String, required: [true, 'Password is required'] }
+const routineSchema = new Schema({
+  name: { type: String, required: [true, "Name is required"] },
+  products: { type: [ObjectId], required: [true, "Products is required"] },
 });
 
-export default model('User', userSchema);
+const userSchema = new Schema({
+  name: { type: String, required: [true, "Name is required"] },
+  email: { type: String, required: [true, "Email is required"] },
+  password: { type: String, required: [true, "Password is required"] },
+  routines: { type: [routineSchema] },
+});
+
+export default model("User", userSchema);
