0fe7fa3b7b07b8aa33c568baeec21e1084a4cafc
Natasha Enriquez <95707854+TashaEnriquez@users.noreply.github.com>
2022-05-09 15:01:54 +0200

    add find product by id, name and brand

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import express from "express";
 //import sessionAuth from "./routes/sessionAuth.js";
-import products from "./controllers/products.js";
+import productsRouter from "./routes/productsRouter.js";
 import ingredientsRouter from "./routes/ingredientsRouter.js";
 import authRouter from "./routes/authRouter.js";
 import routines from "./controllers/routines.js";
 import "./db/index.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5050;
 
 //app.use('/session-auth', sessionAuth);
-app.use("/products", products);
+app.use("/products", productsRouter);
 app.use("/ingredients", ingredientsRouter);
 app.use("/auth", authRouter);
 app.use("/routines", routines);
