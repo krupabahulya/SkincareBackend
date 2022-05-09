75cc39da16337822b323c2a68bb74920c99ff62b
Natasha Enriquez <95707854+TashaEnriquez@users.noreply.github.com>
2022-05-09 18:01:12 +0200

    create edit controller for routines

--- routes/routinesRouter.js
+++ routes/routinesRouter.js
@@ -1,17 +1,22 @@
-/* import { Router } from "express";
-import { getRoutinesForUser, createRoutineForUser } from "../controllers/routines.js";
+import { Router } from "express";
+import {
+  getRoutinesForUser,
+  createRoutineForUser,
+  updateRoutine,
+  deleteRoutine,
+} from "../controllers/routines.js";
+import verifyToken from "../middlewares/verifyToken.js";
 
 const routinesRouter = Router();
 
 routinesRouter
   .route("/user/:userId/routines")
-  .get(getRoutinesForUser)
-  .post(createRoutineForUser);
+  .get(verifyToken, getRoutinesForUser)
+  .post(verifyToken, createRoutineForUser);
 
-// routinesRouter
-//   .route("/user/:userId/routines/:routineId")
-//   .put(updateRoutine)
-//   .delete(deleteRoutine);
+routinesRouter
+  .route("/user/:userId/routines/:routineId")
+  .put(verifyToken, updateRoutine)
+  .delete(verifyToken, deleteRoutine);
 
 export default routinesRouter;
- */
